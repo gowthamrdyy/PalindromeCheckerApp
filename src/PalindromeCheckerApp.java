26e0b8d073d83782cace5d6e65d1537f51c9be18
gowthamrdyy <iamgowthamsree@gmail.com>
2026-02-25 10:25:05 +0530

    UC:2 Sussessfully implementaion of logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,21 @@
+import java.util.*;
 public class PalindromeCheckerApp{
-public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker app");
-        System.out.println("Version 1.0");
-        System.out.println("System Initalzed Successfully");
+public static void main(String[] args) {
+    System.out.println("Welcome to Palindrome Checker app");
+    System.out.println("Version 1.0");
+    System.out.println("System Initalzed Successfully");
+    Scanner a=new Scanner(System.in);
+    System.out.print("Enter a Name:");
+    String name=a.nextLine();
+    int n=name.length();
+    boolean b = true;
+    for(int i=0;i<n/2;i++) {
+        if (name.charAt(i) != name.charAt(n - i-1)) {
+            b = false;
+        } else {
+            b = true;
+        }
+    }
+    System.out.println(b);
     }
 }
