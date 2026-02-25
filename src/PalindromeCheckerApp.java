ad17d8ec1c469fdbb985a4d1fc37975b780bd65d
gowthamrdyy <iamgowthamsree@gmail.com>
2026-02-25 11:31:27 +0530

    Implemented Reverse String Logic

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -17,5 +17,15 @@ public static void main(String[] args) {
         }
     }
     System.out.println(b);
+    String Reverse="";
+    for(int i=n-1;i>=0;i--){
+        Reverse=Reverse+name.charAt(i);
+    }
+    if(name==Reverse){
+        System.out.println("It is Palindrome");
+        }
+    else{
+        System.out.println("It's not a palindrome");
+        }
     }
 }
