49fc7cf8b46eee9866811dea23ab30b313acb7d3
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-03-05 09:33:57 +0530

    Refactor PalindromeCheckerApp by removing old logic
    Removed initial welcome messages and palindrome checking logic.

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,6 @@
 import java.util.*;
 public class PalindromeCheckerApp{
 public static void main(String[] args) {
-    System.out.println("Welcome to Palindrome Checker app");
-    System.out.println("Version 1.0");
-    System.out.println("System Initalzed Successfully");
-    Scanner a=new Scanner(System.in);
-    System.out.print("Enter a Name:");
-    String name=a.nextLine();
-    int n=name.length();
-    boolean b = true;
-    for(int i=0;i<n/2;i++) {
-        if (name.charAt(i) != name.charAt(n - i-1)) {
-            b = false;
-        } else {
-            b = true;
-        }
-    }
-    System.out.println(b);
     String Reverse="";
     for(int i=n-1;i>=0;i--){
         Reverse=Reverse+name.charAt(i);
