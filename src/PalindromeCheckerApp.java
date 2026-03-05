1d778fc529b41894fa61ce5921b4013a7b818fd2
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-03-05 09:33:34 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,9 +1,6 @@
 import java.util.*;
 public class PalindromeCheckerApp{
 public static void main(String[] args) {
-    System.out.println("Welcome to Palindrome Checker app");
-    System.out.println("Version 1.0");
-    System.out.println("System Initalzed Successfully");
     Scanner a=new Scanner(System.in);
     System.out.print("Enter a Name:");
     String name=a.nextLine();
