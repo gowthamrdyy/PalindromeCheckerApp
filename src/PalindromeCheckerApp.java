b45bb90488bd64c964ec740b58076723271c8ea2
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-03-11 23:01:16 +0530

    UC11: Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,41 @@
-public class PalindromeCheckerApp {
+import java.util.Stack;
+
+class PalindromeChecker {
+    
+    public boolean checkPalindrome(String input) {
+        if (input == null) {
+            return false;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        int length = input.length();
+        
+        for (int i = 0; i < length; i++) {
+            stack.push(input.charAt(i));
+        }
+        
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
 
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Welcome to the Object-Oriented Palindrome Checker");
+        System.out.print("Enter a Name: ");
+        String name = scanner.nextLine();
+        
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean isPalindrome = checker.checkPalindrome(name);
+        
+        System.out.println("Input : " + name + "\nIs Palindrome? : " + isPalindrome);
+        scanner.close();
+    }
+}
