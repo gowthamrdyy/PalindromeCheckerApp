7a8e5d1d8bb7c90c7be1f9d062fd08aa308d30f8
Gowtham sree charan reddy <iamgowthamsree@gmail.com>
2026-03-11 23:26:45 +0530

    UC12: Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,73 @@
-public class PalindromeCheckerApp {
+import java.util.Stack;
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+interface PalindromeStrategy {
+    boolean check(String str);
 }
 
+class StackStrategy implements PalindromeStrategy {
+    public boolean check(String str) {
+        if (str == null) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class DequeStrategy implements PalindromeStrategy {
+    public boolean check(String str) {
+        if (str == null) {
+            return false;
+        }
+        Deque<Character> deque = new ArrayDeque<>();
+        for (int i = 0; i < str.length(); i++) {
+            deque.addLast(str.charAt(i));
+        }
+        while (deque.size() > 1) {
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+            if (first != last) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+class PalindromeContext {
+    private PalindromeStrategy strategy;
+
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean executeStrategy(String str) {
+        return strategy.check(str);
+    }
+}
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        
+        PalindromeContext context = new PalindromeContext();
+        
+        context.setStrategy(new StackStrategy());
+        boolean isPalindrome = context.executeStrategy(name);
+        
+        System.out.println("Input : " + name + "\nIs Palindrome? : " + isPalindrome);
+        scanner.close();
+    }
+}
